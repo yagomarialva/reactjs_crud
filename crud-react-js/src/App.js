3373dd890281bcf1e86fc005436b4f5cd2a42152
YagoMarialva <yago.marialva@gmail.com>
2022-02-03 22:13:08 -0400

    Adding template in project

--- crud-react-js/src/App.js
+++ crud-react-js/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header
+ from './components/template/header/Header';
+import Footer from './components/template/footer/footer';
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h2>Dale dele doli</h2>
         </p>
         <a
           className="App-link"
@@ -18,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer/>
     </div>
   );
 }
---
+++ crud-react-js/src/components/template/footer/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <div class="container">
+                <p class="pull-right text-roxo">Created by Yago Marialva</p>
+            </div>
+        </footer>
+    );
+};
+
+
+
+export default Footer;
